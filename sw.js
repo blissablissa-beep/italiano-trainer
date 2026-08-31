e11603413b927f229b50b4b25693049308beeb25
blissablissa-beep <blissablissa@gmail.com>
2026-09-01 08:21:56 +0900

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,59 +1,114 @@
-const CACHE_NAME = "italiano-trainer-v4";
+const CACHE_NAME =
+  "italiano-trainer-v5";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
-  "./data/words.json",
   "./manifest.webmanifest",
-  "./icon.svg"
+  "./icon.svg",
+  "./data/words.json",
+  "./data/words2.json",
+  "./data/words3.json",
+  "./data/words4.json",
+  "./data/words5.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches
-      .keys()
-      .then(keys =>
-        Promise.all(
-          keys
-            .filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+self.addEventListener(
+  "install",
+  event => {
+    event.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(ASSETS)
         )
-      )
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") {
-    return;
+        .then(() =>
+          self.skipWaiting()
+        )
+    );
   }
+);
 
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
+self.addEventListener(
+  "activate",
+  event => {
+    event.waitUntil(
+      caches
+        .keys()
+        .then(keys =>
+          Promise.all(
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+          )
+        )
+        .then(() =>
+          self.clients.claim()
+        )
+    );
+  }
+);
 
-      return fetch(event.request).then(networkResponse => {
-        const copy = networkResponse.clone();
+self.addEventListener(
+  "fetch",
+  event => {
+    const request = event.request;
 
-        caches
-          .open(CACHE_NAME)
-          .then(cache => cache.put(event.request, copy));
+    if (
+      request.method !== "GET" ||
+      new URL(request.url).origin !==
+        self.location.origin
+    ) {
+      return;
+    }
 
-        return networkResponse;
-      });
-    })
-  );
-});
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          if (response.ok) {
+            const copy =
+              response.clone();
+
+            caches
+              .open(CACHE_NAME)
+              .then(cache =>
+                cache.put(
+                  request,
+                  copy
+                )
+              );
+          }
+
+          return response;
+        })
+        .catch(() =>
+          caches
+            .match(request)
+            .then(cachedResponse => {
+              if (cachedResponse) {
+                return cachedResponse;
+              }
+
+              if (
+                request.mode ===
+                "navigate"
+              ) {
+                return caches.match(
+                  "./index.html"
+                );
+              }
+
+              return Response.error();
+            })
+        )
+    );
+  }
+);
